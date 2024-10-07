0164916fd78840dcd25a3274c236b5e601468b39
Dayno15 <alvesferro15@gmail.com>
2024-10-07 18:20:51 -0300

    ajus

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
 <!-- footer.php -->
 <footer>
-    <p>&copy; 2024 Sistema de Assistências ssssssssssssssssssss Técnicas</p>
+    <p>&copy; 2024 Sistema de Assistências Técnicas</p>
 </footer>
 </body>
 </html>
